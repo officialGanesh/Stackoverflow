80eee4f678f65677bb748f0e563b9cb32617018a
Ganesh <ganuofficial2795@gmail.com>
2021-07-10 12:55:54 +0530

    Initial Commit

---
+++ scraper.py
@@ -0,0 +1,14 @@
+import requests, fire
+from requests_html import HTMLSession
+
+stackoverflow = "https://stackoverflow.com/"
+
+def main():
+    """ Main Script File """
+    
+
+
+if __name__ == "__main__":
+
+    fire.Fire(main)
+    print("Code Completed 🔥")
